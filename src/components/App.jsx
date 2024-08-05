85b819436e68308c9673ad32ec4e9e91cb285378
MaYiHsuan <cindy21769897@gmail.com>
2024-08-05 21:06:42 +0800

    feat: change the color of header buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
-import Header from "./Header";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Header'; // 調整路徑根據你的目錄結構
+import Home from './Home';
+import Projects from './Projects';
+import SideProjects from './SideProjects';
+import VisualMerchandising from './VisualMerchandising';
+import Resume from './Resume';
+import About from './About';
 
 function App() {
     return (
-        <div>
+        <Router>
             <Header />
-        </div>
+            <Home />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Projects" element={<Projects />} />
+                <Route path="/SideProjects" element={<SideProjects />} />
+                <Route path="/VisualMerchandising" element={<VisualMerchandising />} />
+                <Route path="/Resume" element={<Resume />} />
+                <Route path="/About" element={<About />} />
+            </Routes>
+        </Router>
     );
 }
 
---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+function Projects() {
+    return (
+        <div>
+            <Header />
+            <h1>Cindy hihi</h1>
+        </div>
+    );
+}
+
+export default Projects;
