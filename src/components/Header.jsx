243f4852243d1f89d6349f92de685b2305f77104
MaYiHsuan <cindy21769897@gmail.com>
2024-08-05 21:06:42 +0800

    feat: change the color of header buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../public/styles.css';
 
 function Header() {
     return (
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-                <span class="fs-4">Simple header</span>
-            </a>
+        <div className="container">
+            <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+                <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                    <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
+                    <span className="fs-4">Cindy's Portfolio</span>
+                </Link>
 
-            <ul class="nav nav-pills">
-                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-            </ul>
-        </header>
+                <ul className="nav nav-pills">
+                    <li className="nav-item"><Link to="/" className="nav-link active" aria-current="page">Home</Link></li>
+                    <li className="nav-item"><Link to="/Projects" className="nav-link">Projects</Link></li>
+                    <li className="nav-item"><Link to="/SideProjects" className="nav-link">Side Projects</Link></li>
+                    <li className="nav-item"><Link to="/VisualMerchandising" className="nav-link">Visual Merchandising</Link></li>
+                    <li className="nav-item"><Link to="/Resume" className="nav-link">Resume</Link></li>
+                    <li className="nav-item"><Link to="/About" className="nav-link">About</Link></li>
+                </ul>
+            </header>
+        </div>
     );
 }
 
